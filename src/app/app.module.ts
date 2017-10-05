f5a6fb876261460e9072d8e061ff9391d349dd23
gaoHB <1286513426@qq.com>
2017-10-05 23:10:28 +0800

    update( project ):迁移到分支

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TemplateInterporationComponent } from './template-interporation/template-interporation.component';
-import { TemplateRefVarComponent } from './template-ref-var/template-ref-var.component';
-import { TemplateValueBindComponent } from './template-value-bind/template-value-bind.component';
-import { TemplateEventBindingComponent } from './template-event-binding/template-event-binding.component';
-import { TemplateTwoWayBindingComponent } from './template-two-way-binding/template-two-way-binding.component';
-import { FontResizerComponent } from './template-two-way-binding/font-resizer/font-resizer.component';
-import { TemplateNgIfComponent } from './template-ng-if/template-ng-if.component';
-import { TemplateNgForComponent } from './template-ng-for/template-ng-for.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TemplateInterporationComponent,
-    TemplateRefVarComponent,
-    TemplateValueBindComponent,
-    TemplateEventBindingComponent,
-    TemplateTwoWayBindingComponent,
-    FontResizerComponent,
-    TemplateNgIfComponent,
-    TemplateNgForComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
