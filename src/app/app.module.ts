7c8832fa1e9b76a6a02eb05cf8176d0505f8d5a5
gaoHB <1286513426@qq.com>
2017-10-06 23:34:10 +0800

    add( LifeCycle ):组件生命周期

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule  } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { LifeCycleComponent } from './life-cycle/life-cycle.component';
+import { TestNgOnchangesComponent } from './test-ng-onchanges/test-ng-onchanges.component';
+import { Child111Component } from './test-ng-onchanges/child111/child111.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    LifeCycleComponent,
+    TestNgOnchangesComponent,
+    Child111Component
+],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/life-cycle/life-cycle.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit, OnChanges, ChangeDetectionStrategy, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-life-cycle',
+  templateUrl: './life-cycle.component.html',
+  styleUrls: ['./life-cycle.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LifeCycleComponent implements OnInit, OnChanges {
+  public logs: Array<string> = [];
+
+  @Input()
+  public title: string = '';
+
+  constructor() {
+    /**
+     * 构造函数里面最好只做一些简单的赋值操作，不要做复杂的逻辑
+     * 获取服务端数据、业务逻辑等不要实现在构造函数里面
+     * 想知道详细的原因请看这篇解释http://misko.hevery.com/code-reviewers-guide/flaw-constructor-does-real-work/
+     */
+    this.logs.push('constructor...');
+  }
+
+  ngOnChanges() {
+    this.logs.push('ngOnChanges...');
+  }
+
+  ngOnInit() {
+    this.logs.push('ngOnInit...');
+  }
+
+  ngDoCheck() {
+    this.logs.push('ngDoCheck...');
+  }
+
+  ngAfterContentInit() {
+    this.logs.push('ngAfterContentInit...');
+  }
+
+  ngAfterContentChecked() {
+    this.logs.push('ngAfterContentChecked...');
+  }
+
+  ngAfterViewInit() {
+    this.logs.push('ngAfterViewInit...');
+  }
+
+  ngAfterViewChecked() {
+    this.logs.push('ngAfterViewChecked...');
+  }
+
+  ngOnDestory() {
+    this.logs.push('ngOnDestory...');
+  }
+
+}
