d8fe6bb191d8d00b890b501ed0b55a9409dced45
gaoHB <1286513426@qq.com>
2017-10-08 18:20:05 +0800

    add( animation ):添加动效

---
+++ src/app/test-fly-in/test-fly-in.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { TestFlyInComponent } from './test-fly-in.component';
+
+describe('TestFlyInComponent', () => {
+  let component: TestFlyInComponent;
+  let fixture: ComponentFixture<TestFlyInComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TestFlyInComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestFlyInComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
